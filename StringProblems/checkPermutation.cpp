551941bad67d2a6a091c028039614823eef187a3
carlinhosmugiwara <55931229+carlinhosmugiwara@users.noreply.github.com>
2020-12-28 16:28:14 -0300

    Update checkPermutation.cpp

--- StringProblems/checkPermutation.cpp
+++ StringProblems/checkPermutation.cpp
@@ -2,8 +2,10 @@
 
 using namespace std;
 
+// C++ program to check if two strings are a permutation of each other
+
 string sortString(string &input){
-	sort(input.begin(), input.end());
+	sort(input.begin(), input.end()); // Built in function to sort out the string
 	
 	return input;
 
@@ -16,7 +18,7 @@ int main(){
 	cin>>input1;
 	cin>>input2;
 	
-	if(sortString(input1) == sortString(input2)){
+	if(input1 != input2 && sortString(input1) == sortString(input2)){ // The logic is: if two strings are different, but equal when sorted, them they're permutations
 		cout<<"One is the permutation of the other and vice-versa";
 	}
 	else {
